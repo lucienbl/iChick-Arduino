d07f61a3c82a4027704829a7ba5bb1dc3b962f23
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2019-12-02 18:19:05 +0100

    feat: added water sensor & dht11 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include "RestApi.h"
+#include <dht11.h>
 
-int LED = 4;
+#define WATER_SENSOR A0
+#define LED 4
+#define DHT11_PIN 3
 
 RestApi restApi(80);
+dht11 DHT11;
 
 String led(char *params = "") {
     if (String(params).indexOf("on") != -1) {
@@ -15,21 +19,31 @@ String led(char *params = "") {
     }
 }
 
+String waterLevel(char *params = "") {
+    return "{\"value\": " + String(analogRead(WATER_SENSOR)) + "}";
+}
+
+String dht11(char *params = "") {
+    return "{\"humidity\": " + String((float) DHT11.humidity, 2) + ", \"temperature\": " + String((float) DHT11.temperature, 2) + "}";
+}
+
 void setup() {
     // Start serial monitor
     Serial.begin(9600);
-
-    // Define pins here
-    pinMode(LED, OUTPUT);
 
     // Initialize server
     restApi.init();
 
     // Add routes
     restApi.addRoute(GET, "/led", &led);
+    restApi.addRoute(GET, "/waterLevel", &waterLevel);
+    restApi.addRoute(GET, "/dht11", &dht11);
 }
 
 void loop() {
     // Handle HTTP Connections
     restApi.run();
+
+    // Read temp
+    DHT11.read(DHT11_PIN);
 }
