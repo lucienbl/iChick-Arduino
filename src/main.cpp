0f60ffb9bf6d4d03d05803d89ca7ab2eda00a157
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-03-07 16:43:07 +0100

    feat: added i2c door control

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
 #include "RestApi.h"
 #include <dht11.h>
+#include <Wire.h>
 
+// Ports
 #define WATER_SENSOR A0
 #define LED 4
 #define DHT11_PIN 3
 
+// Actions
+#define ACTION_DOOR_OPEN 1
+#define ACTION_DOOR_CLOSE 2
+
+// Cards
+#define CARD_MAIN_2 4
+
 RestApi restApi(80);
 dht11 DHT11;
+
+
+void i2cTransmit(int address, int data) {
+    Wire.beginTransmission(address);
+    Wire.write(data);
+    Wire.endTransmission();
+}
 
 String led(char *params = "") {
     if (String(params).indexOf("on") != -1) {
@@ -27,9 +43,24 @@ String dht11(char *params = "") {
     return "{\"humidity\": " + String((float) DHT11.humidity, 2) + ", \"temperature\": " + String((float) DHT11.temperature, 2) + "}";
 }
 
+String openDoor(char *params = "") {
+    i2cTransmit(CARD_MAIN_2, ACTION_DOOR_OPEN);
+
+    return String(R"({"success": true})");
+}
+
+String closeDoor(char *params = "") {
+    i2cTransmit(CARD_MAIN_2, ACTION_DOOR_CLOSE);
+
+    return String(R"({"success": true})");
+}
+
 void setup() {
     // Start serial monitor
     Serial.begin(9600);
+
+    // Start card serial
+    Wire.begin();
 
     // Initialize server
     restApi.init();
@@ -38,6 +69,8 @@ void setup() {
     restApi.addRoute(GET, "/led", &led);
     restApi.addRoute(GET, "/waterLevel", &waterLevel);
     restApi.addRoute(GET, "/dht11", &dht11);
+    restApi.addRoute(GET, "/openDoor", &openDoor);
+    restApi.addRoute(GET, "/closeDoor", &closeDoor);
 }
 
 void loop() {
